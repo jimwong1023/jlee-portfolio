c77a8ca6869e3d382d272baecebbb4fc20b27c03
jimwong1023 <jimwong1023@gmail.com>
2016-07-17 22:49:11 -0700

    cs2

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,10 @@ class StaticPagesController < ApplicationController
 
   def about
   end
+
+  def cs1
+  end
+
+  def cs2
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "static_pages#homepage"
   get "about", to: "static_pages#about"
+  get "cs1", to: "static_pages#cs1"
+  get "cs2", to: "static_pages#cs2"
 end
