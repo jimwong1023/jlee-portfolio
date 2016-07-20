68b809bee49650a672c75c32abcb402ffb26b303
Jimmy <jimwong1023@gmail.com>
2016-07-20 10:36:50 -0700

    mobile bg img

--- app/controllers/download_controller.rb
+++ app/controllers/download_controller.rb
@@ -1,5 +1,5 @@
 class DownloadController < ApplicationController
   def resume
-    send_file "#{Rails.root}/public/jlee_resume.pdf", filename: 'jlee_resume.pdf', type: "application/pdf", disposition: 'inline'
+    send_file "#{Rails.root}/public/Jlee_resume.pdf", filename: 'Jlee_resume.pdf', type: "application/pdf", disposition: 'inline'
   end
 end
